47efa2f06f114a455bb365367a9905a8e37f0966
msalam <ms17alam@gmail.com>
2018-04-09 14:11:41 -0400

    aall files are set up

---
+++ src/app.js
@@ -0,0 +1,10 @@
+console.log("app.js is runnig");
+
+// JSX = Javascript XML
+
+var template = <h1> Inside App</h1>;
+
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
