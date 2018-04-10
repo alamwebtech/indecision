779ec007a5f116a0d79b5eb3290d8f37b87828c6
msalam <ms17alam@gmail.com>
2018-04-10 15:12:11 -0400

    My own Template Works

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,20 +5,70 @@ console.log("app.js is runnig");
 // JSX = Javascript XML
 
 var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    " Inside App"
-  ),
-  React.createElement(
-    "p",
-    null,
-    " This is some text from the APP"
-  )
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		" Inside Appliation"
+	),
+	React.createElement(
+		"p",
+		null,
+		" This is some text from the APPLICATION"
+	),
+	React.createElement(
+		"ol",
+		null,
+		React.createElement(
+			"li",
+			null,
+			"Item One"
+		),
+		React.createElement(
+			"li",
+			null,
+			"Item Two"
+		)
+	),
+	React.createElement(
+		"ul",
+		null,
+		React.createElement(
+			"li",
+			null,
+			"Number One"
+		),
+		React.createElement(
+			"li",
+			null,
+			"Number Two"
+		)
+	)
+);
+
+var myTemplate = React.createElement(
+	"div",
+	null,
+	React.createElement(
+		"h2",
+		null,
+		"Creating My Own Template"
+	),
+	React.createElement(
+		"p",
+		null,
+		"I am at 30"
+	),
+	React.createElement(
+		"p",
+		null,
+		"I live in Delaware"
+	)
 );
 
 var appRoot = document.getElementById("app");
+var myRoot = document.querySelector(".myApp");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(myTemplate, appRoot);
+ReactDOM.render(template, myRoot);
--- src/app.js
+++ src/app.js
@@ -2,11 +2,34 @@ console.log("app.js is runnig");
 
 // JSX = Javascript XML
 
-var template = <div>
-	<h1> Inside App</h1>
-    <p> This is some text from the APP</p></div>;
+var template = (
+	<div>
+	<h1> Inside Appliation</h1>
+    <p> This is some text from the APPLICATION</p>
+	
+	<ol>
+	<li>Item One</li>
+	<li>Item Two</li>
+	</ol>
+	<ul>
+	<li>Number One</li>
+	<li>Number Two</li>
+	</ul>
+	
+	</div>);
+	
+	var myTemplate = (
+	<div>
+	<h2>Creating My Own Template</h2>
+	<p>I am at 30</p>
+	<p>I live in Delaware</p>
+	</div>
 
+);
+	
 
 var appRoot = document.getElementById("app");
+var myRoot = document.querySelector(".myApp");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(myTemplate, appRoot);
+ReactDOM.render(template, myRoot);
