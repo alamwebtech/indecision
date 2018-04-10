e955bad3fed21b0f67cecf0e38199a264a3dd6b4
msalam <ms17alam@gmail.com>
2018-04-10 16:03:36 -0400

    usiing object in React

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -47,23 +47,40 @@ var template = React.createElement(
 	)
 );
 
+//	var userName = "MD ALAM";
+//	var sentence = "Creating My Own Template";
+//	var ag = 30;
+//	var loc= "DE USA";
+
+var user = {
+	userName: "MD ALAM",
+	sentence: "Creating my own Templates",
+	ag: 30,
+	loc: "Delaware"
+};
+
 var myTemplate = React.createElement(
 	"div",
 	null,
 	React.createElement(
+		"h1",
+		null,
+		user.userName
+	),
+	React.createElement(
 		"h2",
 		null,
-		"Creating My Own Template"
+		user.sentence.toUpperCase()
 	),
 	React.createElement(
 		"p",
 		null,
-		"I am at 30"
+		user.ag
 	),
 	React.createElement(
 		"p",
 		null,
-		"I live in Delaware"
+		user.loc
 	)
 );
 
