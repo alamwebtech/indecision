27e04c4312c4302b8156cb92729a558570ea8177
msalam <ms17alam@gmail.com>
2018-04-13 15:17:52 -0400

    using forEach

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,3 +11,22 @@ var arrow = function arrow(x) {
 };
 
 console.log(arrow(10));
+
+var firstName = function firstName(fullName) {
+	return fullName.split(" ")[0];
+};
+
+console.log(firstName("MD ALAM"));
+
+var user = {
+	name: "MD ALAM",
+	cities: ["NY", "PHIL", "WIL", "BOS"],
+	printPlaceLived: function printPlaceLived() {
+		var cityName = this;
+		this.cities.forEach(function (city) {
+			console.log(cityName.name + " has lived in " + city);
+		});
+	}
+};
+
+user.printPlaceLived();
--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -8,3 +8,22 @@ const arrow = (x)=> x * x;
 
 
 console.log (arrow(10));
+
+
+const firstName = (fullName) => fullName.split(" ")[0];
+
+console.log(firstName("MD ALAM"));
+
+
+const user = {
+	name: "MD ALAM",
+	cities: ["NY", "PHIL", "WIL", "BOS"],
+	printPlaceLived : function (){
+	const cityName = this;
+	this.cities.forEach(function(city){
+	  console.log(cityName.name + " has lived in " + city);
+    });
+  }
+};
+
+user.printPlaceLived();
