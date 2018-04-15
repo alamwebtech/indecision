f071f00c4445f83f80cfaafa807e8a619ca3d592
msalam <ms17alam@gmail.com>
2018-04-15 14:32:28 -0400

    App Works

--- src/app.js
+++ src/app.js
@@ -19,31 +19,31 @@ var template = (
 	</div>);
 	
 	
-
-
 let add =0;
 
 const addOne = () => {
-	console.log("addOne");
+	add = add + 5;
+	increment();
 };
-
-	const templateTwo = (
-	
-	<div>
-		
-		<h1>Add Five each time: {add}</h1>
-		
-		<button onClick={addOne}> +1 Each Time</button>
-	</div>
-		
-	
-	);
-	
-console.log(templateTwo);
 
 	const appTwo = document.getElementById("myApp");
 	const appRoot = document.getElementById("app");
-
-
 	ReactDOM.render(template, appRoot);
+	
+
+const increment = () =>{
+	const templateTwo = (
+	
+	<div>
+		<h1>Add Five each time: {add}</h1>
+		
+		<button onClick={addOne}> +5 Each Time</button>
+	</div>
+
+	);
+	
 	ReactDOM.render(templateTwo, appTwo);
+	
+};
+
+increment();
