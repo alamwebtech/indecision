51d7b2ec7846b76a090af9467ee65db7e5cbdeaa
msalam <ms17alam@gmail.com>
2018-04-16 16:31:49 -0400

    trying to fix the problem

--- src/app.js
+++ src/app.js
@@ -2,23 +2,49 @@ console.log("app.js is runnig");
 
 // JSX = Javascript XML
 
-var template = (
+const app = {
+	title: "Indecission App",
+	subtitle: "Put Your life in the handle of a computer",
+	options: []
+};
+
+const onFormSubmit = (e)=>{
+	e.preventDefault();
+	
+	const option = e.target.elements.option.value;
+	if (option){
+		app.options.push(option);
+		e.target.elements.option.value = '';
+		appRender();
+	}
+};
+
+
+	const myApp = document.querySelector("#app");
+
+const appRender = () => {
+	const template = (
 	<div>
-	<h1> Inside Appliation</h1>
-    <p> This is some text from the APPLICATION</p>
+	<h1>{app.title}</h1>
+	{app.subtitle && <p>{app.subtitle}</p>}
+	<p>{app.options.length > 0 ? "Here are your Options" : "No Options"}</p>
+	
+	<p> {app.options.length} </P>
+   
 	
 	<ol>
 	<li>Item One</li>
 	<li>Item Two</li>
 	</ol>
-	<ul>
-	<li>Number One</li>
-	<li>Number Two</li>
-	</ul>
-	
-	</div>);
-	
-	
-	const myApp = document.querySelector(".app");
+		
+    <form onSubmit={onFormSubmit}>
+	<input type='text' name='option'/>
+	<button>Add Option</button>
+	</form>
+	</div>
+	);
 	
 	ReactDOM.render(template, myApp);
+};
+
+appRender();
