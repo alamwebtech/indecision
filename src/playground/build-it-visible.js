0f99a621eaa2b3b3fc2733852c6e32497d586028
msalam <ms17alam@gmail.com>
2018-04-22 14:04:39 -0400

    toggeling button works

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,7 +1,23 @@
+let visibility = false;
+
+const toggleVisibility = () => {
+	visibility = !visibility;
+	render();
+};
+
 const render = () => {
 	const jsx = (
 		<div>
 		<h1>Visibility Toggle</h1>
+		<button onClick={toggleVisibility}>
+		{visibility ? "Hide Details" : "Show Details"}
+		</button>
+		{visibility && (
+	<div>
+		<p>Some Details you can now see..!!</p>
+	</div>
+	
+	)}
 		</div>
 	);
 	ReactDOM.render(jsx, document.getElementById('app'));
